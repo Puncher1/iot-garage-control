0139fdae8d41e30074609fe341a7e002f7ba1c27
Puncher <65789180+Puncher1@users.noreply.github.com>
2023-11-20 11:23:16 +0100

    Started with wifi initialization

--- iot-garage-control/src/webserver.cpp
+++ iot-garage-control/src/webserver.cpp
@@ -1,4 +1,20 @@
 #include <WiFi.h>
 
+const char* ssid = "iPhone 12 Pro";
+const char* password = "1234gggg";
+
+WiFiServer server(80);
+
+void initWiFi()
+{
+    Serial.println("Connecting to ");
+    Serial.println(ssid);
+ 
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED){}
+
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
 
 
