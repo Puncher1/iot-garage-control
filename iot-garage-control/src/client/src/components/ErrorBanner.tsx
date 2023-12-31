0c5474485611af02288e7fe0b3493f6694a4d639
Puncher <65789180+Puncher1@users.noreply.github.com>
2023-12-31 13:36:31 +0100

    Add shadow to error banner + change error icon

--- iot-garage-control/src/client/src/components/ErrorBanner.tsx
+++ iot-garage-control/src/client/src/components/ErrorBanner.tsx
@@ -12,7 +12,9 @@ interface ErrorBannerPropsType {
 export default function ErrorBanner({ children, onRetry, errorType }: ErrorBannerPropsType) {
   return (
     <div role="alert" className="error-banner">
-      <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-7 w-7" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+      </svg>
       <span className="error-message">{children}</span>
       {errorType == ErrorType.unexpected &&
         <button className="btn btn-xs sm:btn-sm" onClick={() => onRetry(true)}>Retry</button>
