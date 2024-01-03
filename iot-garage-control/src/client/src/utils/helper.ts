e6225f2fe8962b767d2b7c4d029386f6f5b95d68
Puncher <65789180+Puncher1@users.noreply.github.com>
2024-01-03 17:51:42 +0100

    Remove unneeded helper function + add stringformat

--- iot-garage-control/src/client/src/utils/helper.ts
+++ iot-garage-control/src/client/src/utils/helper.ts
@@ -1,32 +1,9 @@
-import { ErrorType } from "./enums"
 
-
-interface errorBannerReturnType {
-    isErrorBanner: boolean,
-    errorType: ErrorType | null,
-    errorMessage: string,
-    bannerSizeClass: string
-}
-
-export function errorBanner(isError: boolean, isOnline: boolean): errorBannerReturnType {
-    let isErrorBanner = false
-    let errorMessage = ""
-    let errorType = null
-    let bannerSizeClass = ""
-
-    if (!isOnline || isError) {
-        isErrorBanner = true
-        if (!isOnline) {
-            errorType = ErrorType.offline
-            errorMessage = "You're offline! Make sure you have an internet connection."
-            bannerSizeClass = "error-banner-offline"
-        }
-        else if (isError) {
-            errorType = ErrorType.receiving
-            errorMessage = "Connection to server lost, please try again."
-            bannerSizeClass = "error-banner-unexpected"
-        }
-    }
-
-    return { isErrorBanner, errorType, errorMessage, bannerSizeClass }
-}
+export function stringFormat(str: string, ...args: any[]) {
+    return str.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
+};
