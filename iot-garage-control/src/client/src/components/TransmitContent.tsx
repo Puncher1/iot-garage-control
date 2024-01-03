375000b1b2c2dd9ea80ab4e04e9713f02a0c62a1
Puncher <65789180+Puncher1@users.noreply.github.com>
2024-01-03 17:08:06 +0100

    Fix table children have same key

--- iot-garage-control/src/client/src/components/TransmitContent.tsx
+++ iot-garage-control/src/client/src/components/TransmitContent.tsx
@@ -24,10 +24,11 @@ function TransmitContent({ title }: TransmitContentType) {
 
   const rows = model["rows"].map((row, i) => {
     const options = model["options"][i]
+    const uniqueKeyPrefix = `${title}-${i}`
     return (
-      <tr key={title}>
-        <th key={`${title}_h`}>{row}</th>
-        <td key={`${title}_d`}>
+      <tr key={uniqueKeyPrefix}>
+        <th key={`${uniqueKeyPrefix}-h`}>{row}</th>
+        <td key={`${uniqueKeyPrefix}-d`}>
           <select
             className="select select-bordered w-full max-w-xs"
             value={option}
@@ -35,12 +36,12 @@ function TransmitContent({ title }: TransmitContentType) {
           >
             {
               options.map((optionText, i) => (
-                <option value={enumType[i]}>{optionText}</option>
+                <option key={`${uniqueKeyPrefix}-d-${i}`} value={enumType[i]}>{optionText}</option>
               ))
             }
           </select>
         </td>
-        <td key={`${title}_btn`}>
+        <td key={`${title}-btn`}>
           <button className="btn-send" onClick={() => handleRequest(option)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-primary">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
