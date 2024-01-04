6d158b05a9045440e58b9e70f44bb5a557965d78
Puncher <65789180+Puncher1@users.noreply.github.com>
2024-01-04 17:06:11 +0100

    Add back file with new name

---
+++ iot-garage-control/src/client/src/providers/ErrorProvider.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+import { ErrorObjType } from "../utils/types"
+import ErrorContext from "../contexts/errorContext"
+import { ErrorType } from "../utils/enums"
+
+
+interface ErrorProvParamsType {
+  children: React.ReactElement
+}
+
+export default function ErrorProvider({ children }: ErrorProvParamsType) {
+  const [_error, _setError] = useState<ErrorObjType | null>(null)
+
+  const removeError = (type: ErrorType) => {
+    if (!type) {
+      _setError(null)
+    }
+    else if (_error && type == _error.type) {
+      _setError(null)
+    }
+  }
+  const setError = (message: string, type: ErrorType) => {
+    _setError({ message, type })
+  }
+
+  const contextValue = {
+    error: _error,
+    setError,
+    removeError
+  }
+
+  return (
+    <ErrorContext.Provider value={contextValue}>
+      {children}
+    </ErrorContext.Provider>
+  )
+}
