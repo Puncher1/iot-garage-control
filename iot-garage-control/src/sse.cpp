cec7440af499ed82da17164262add81ce951e29e
Puncher <65789180+Puncher1@users.noreply.github.com>
2024-01-15 15:02:14 +0100

    Send not ready error

--- iot-garage-control/src/sse.cpp
+++ iot-garage-control/src/sse.cpp
@@ -11,15 +11,21 @@ void SSEConnectEvent(AsyncEventSourceClient* client)
 
 void SSEHandler(AsyncEventSource* eventSource, BoardCom::RX data) 
 {  
-    DynamicJsonDocument jsonObj(1024);
-    jsonObj["auth"]["last_login"] = data.lastLogin;
-    jsonObj["gate_control"]["status"] = static_cast<int>(data.isGate_open);
-    jsonObj["co2_meas"]["status"] = static_cast<int>(data.isCO2_ok);
-    jsonObj["air_meas"]["status"] = static_cast<int>(data.isAir_ok);
-    jsonObj["air_control"]["status"] = data.airControl;
+    if (data.isReady) 
+    {
+        DynamicJsonDocument jsonObj(1024);
+        jsonObj["auth"]["last_login"] = data.lastLogin;
+        jsonObj["gate_control"]["status"] = static_cast<int>(data.isGate_open);
+        jsonObj["co2_meas"]["status"] = static_cast<int>(data.isCO2_ok);
+        jsonObj["air_meas"]["status"] = static_cast<int>(data.isAir_ok);
+        jsonObj["air_control"]["status"] = data.airControl;
 
-    String jsonString;
-    serializeJson(jsonObj, jsonString);
-    
-    eventSource->send(jsonString.c_str(), "data", millis());
+        String jsonString;
+        serializeJson(jsonObj, jsonString);
+        eventSource->send(jsonString.c_str(), "data", millis());
+    }
+    else 
+    {
+        eventSource->send("not_ready", "error", millis());
+    }
 }
